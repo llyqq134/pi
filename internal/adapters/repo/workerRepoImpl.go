9716c384c32a17421d0ecdf9bf63f3630b16549b
lly <llyqq134@gmail.com>
2026-02-03 16:01:47 +0300

    repo implementation

--- internal/adapters/repo/workerRepoImpl.go
+++ internal/adapters/repo/workerRepoImpl.go
@@ -29,7 +29,7 @@ func (r *WorkerRepoImpl) Create (ctx context.Context, worker *entities.Worker) e
 	`
 
 	if err := r.Client.QueryRow(ctx, query, 
-		&worker.Name, &worker.JobTitle, &worker.Departament, &worker.HashPass, &worker.AcessLevel).Scan(&worker.UUID);
+		&worker.Name, &worker.JobTitle, &worker.Department, &worker.HashPass, &worker.AcessLevel).Scan(&worker.UUID);
 		err != nil {
 			if pgErr, ok := err.(*pgconn.PgError); ok {
 				log.Printf("SQL Error: %s\nDetail: %s\nWhere: %s\nCode: %s\nSQL state: %s",
@@ -45,5 +45,77 @@ func (r *WorkerRepoImpl) Create (ctx context.Context, worker *entities.Worker) e
 }
 
 func (r *WorkerRepoImpl) GetByUUID(ctx context.Context, uuid string) (entities.Worker, error) {
-	
+	query := `
+		SELECT id, name, jobtitle, department, hashpass, accesslevel FROM workers WHERE id = $1
+	`	
+	var worker entities.Worker
+
+	if err := r.Client.QueryRow(ctx, query, uuid).Scan(
+		&worker.UUID, &worker.Name, &worker.JobTitle, &worker.HashPass, &worker.AcessLevel); err != nil {
+			if pgErr, ok := err.(*pgconn.PgError); ok {
+				log.Printf("SQL Error: %s\nDetail: %s\nWhere: %s\nCode: %s\nSQL state: %s",
+					pgErr.Message, pgErr.Detail, pgErr.Where, pgErr.Code, pgErr.SQLState())
+
+					return entities.Worker{}, nil
+			}
+		return entities.Worker{}, err
+	}
+
+	return worker, nil
+}
+
+func (r * WorkerRepoImpl) GetAll(ctx context.Context) ([]entities.Worker, error) {
+	query := `
+	SELECT id, name, jobtitle, department, hashpass, accesslevel FROM workers LIMIT 1000
+	`
+	rows, err := r.Client.Query(ctx, query)
+
+	if err != nil {
+		log.Printf("Error querying workers: %v\n", err)
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	workers := make([]entities.Worker, 0)
+
+	for rows.Next() {
+		var worker entities.Worker
+		if err := rows.Scan(&worker.UUID, &worker.Name, &worker.JobTitle, &worker.Department, &worker.AcessLevel);
+			err != nil {
+				log.Printf("err scanning workers: %v\n", err)
+				return nil, err
+			}
+			workers = append(workers, worker)
+	}
+
+	return workers, err
 } 
+
+func (r *WorkerRepoImpl) GetAllByDepartment(ctx context.Context, department string) ([]entities.Worker, error){
+	query := `
+	SELECT id, name, jobtitle, department, hashpass, accesslevel FROM workers WHERE department = $1
+	`
+	rows, err := r.Client.Query(ctx, query, department)
+
+	if err != nil {
+		log.Printf("error quering workers by department: %v\n", err)
+		return nil, err
+	}
+
+	workers := make([]entities.Worker, 0)
+
+	for rows.Next() {
+		var worker entities.Worker
+		if err := rows.Scan(
+			&worker.UUID, &worker.Name, &worker.JobTitle, &worker.Department, &worker.HashPass, &worker.AcessLevel,
+		); err != nil {
+			log.Printf("error scanning workers: %v\n", err)
+			return workers, err
+		}
+
+		workers = append(workers, worker)
+	}
+
+	return workers, err
+}
