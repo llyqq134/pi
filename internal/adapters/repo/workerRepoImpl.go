86ba39212a083b14f02b187714995e61dbe769c4
lly <pinklywmp@gmail.com>
2026-02-03 16:19:08 +0300

    fully implemented

--- internal/adapters/repo/workerRepoImpl.go
+++ internal/adapters/repo/workerRepoImpl.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"log"
 	"pi/internal/app/entities"
-	"pi/internal/app/interfaces/repoInterfaces"
 	"pi/pkg/db"
 
 	"github.com/jackc/pgx/v5/pgconn"
@@ -14,13 +13,13 @@ type WorkerRepoImpl struct {
 	Client db.Client
 }
 
-func NewWorkerImpl(client db.Client) repointerfaces.WorkerRepo {
+func NewWorkerImpl(client db.Client) WorkerRepoImpl {
 	return WorkerRepoImpl{
-	Client: client,
+		Client: client,
 	}
 }
 
-func (r *WorkerRepoImpl) Create (ctx context.Context, worker *entities.Worker) error {
+func (r *WorkerRepoImpl) Create(ctx context.Context, worker *entities.Worker) error {
 	query := `
 	INSERT INTO
 	workers (name, jobtitle, departament, hashpass, accesslevel)
@@ -28,43 +27,43 @@ func (r *WorkerRepoImpl) Create (ctx context.Context, worker *entities.Worker) e
 	RETURNING id
 	`
 
-	if err := r.Client.QueryRow(ctx, query, 
-		&worker.Name, &worker.JobTitle, &worker.Department, &worker.HashPass, &worker.AcessLevel).Scan(&worker.UUID);
-		err != nil {
-			if pgErr, ok := err.(*pgconn.PgError); ok {
-				log.Printf("SQL Error: %s\nDetail: %s\nWhere: %s\nCode: %s\nSQL state: %s",
-					pgErr.Message, pgErr.Detail, pgErr.Where, pgErr.Code, pgErr.SQLState())
+	if err := r.Client.QueryRow(ctx, query,
+		&worker.Name, &worker.JobTitle, &worker.Department, &worker.HashPass, &worker.AcessLevel).
+		Scan(&worker.UUID); err != nil {
+		if pgErr, ok := err.(*pgconn.PgError); ok {
+			log.Printf("SQL Error: %s\nDetail: %s\nWhere: %s\nCode: %s\nSQL state: %s",
+				pgErr.Message, pgErr.Detail, pgErr.Where, pgErr.Code, pgErr.SQLState())
 
-					return nil
-			}
-
-			return err
+			return nil
 		}
 
-		return nil
+		return err
+	}
+
+	return nil
 }
 
 func (r *WorkerRepoImpl) GetByUUID(ctx context.Context, uuid string) (entities.Worker, error) {
 	query := `
 		SELECT id, name, jobtitle, department, hashpass, accesslevel FROM workers WHERE id = $1
-	`	
+	`
 	var worker entities.Worker
 
 	if err := r.Client.QueryRow(ctx, query, uuid).Scan(
 		&worker.UUID, &worker.Name, &worker.JobTitle, &worker.HashPass, &worker.AcessLevel); err != nil {
-			if pgErr, ok := err.(*pgconn.PgError); ok {
-				log.Printf("SQL Error: %s\nDetail: %s\nWhere: %s\nCode: %s\nSQL state: %s",
-					pgErr.Message, pgErr.Detail, pgErr.Where, pgErr.Code, pgErr.SQLState())
+		if pgErr, ok := err.(*pgconn.PgError); ok {
+			log.Printf("SQL Error: %s\nDetail: %s\nWhere: %s\nCode: %s\nSQL state: %s",
+				pgErr.Message, pgErr.Detail, pgErr.Where, pgErr.Code, pgErr.SQLState())
 
-					return entities.Worker{}, nil
-			}
+			return entities.Worker{}, nil
+		}
 		return entities.Worker{}, err
 	}
 
 	return worker, nil
 }
 
-func (r * WorkerRepoImpl) GetAll(ctx context.Context) ([]entities.Worker, error) {
+func (r *WorkerRepoImpl) GetAll(ctx context.Context) ([]entities.Worker, error) {
 	query := `
 	SELECT id, name, jobtitle, department, hashpass, accesslevel FROM workers LIMIT 1000
 	`
@@ -81,18 +80,17 @@ func (r * WorkerRepoImpl) GetAll(ctx context.Context) ([]entities.Worker, error)
 
 	for rows.Next() {
 		var worker entities.Worker
-		if err := rows.Scan(&worker.UUID, &worker.Name, &worker.JobTitle, &worker.Department, &worker.AcessLevel);
-			err != nil {
-				log.Printf("err scanning workers: %v\n", err)
-				return nil, err
-			}
-			workers = append(workers, worker)
+		if err := rows.Scan(&worker.UUID, &worker.Name, &worker.JobTitle, &worker.Department, &worker.AcessLevel); err != nil {
+			log.Printf("err scanning workers: %v\n", err)
+			return nil, err
+		}
+		workers = append(workers, worker)
 	}
 
 	return workers, err
-} 
+}
 
-func (r *WorkerRepoImpl) GetAllByDepartment(ctx context.Context, department string) ([]entities.Worker, error){
+func (r *WorkerRepoImpl) GetAllByDepartment(ctx context.Context, department string) ([]entities.Worker, error) {
 	query := `
 	SELECT id, name, jobtitle, department, hashpass, accesslevel FROM workers WHERE department = $1
 	`
@@ -118,4 +116,34 @@ func (r *WorkerRepoImpl) GetAllByDepartment(ctx context.Context, department stri
 	}
 
 	return workers, err
+}
+
+func (r *WorkerRepoImpl) Update(ctx context.Context, worker *entities.Worker) error {
+	query := `
+		UPDATE workers SET name = $2, jobtitle = $3, department = $4, hashpass = $5, accesslevel = $6
+		WHERE id = $1
+	`
+
+	if _, err := r.Client.Exec(ctx, query, &worker.UUID, &worker.Name, &worker.JobTitle, &worker.Department, &worker.HashPass, &worker.AcessLevel); err != nil {
+		log.Printf("error updating worker: %v\n", err)
+
+		return err
+	}
+
+	return nil
+}
+
+func (r *WorkerRepoImpl) DeleteByUUID(ctx context.Context, uuid string) error {
+	query := `
+		DELETE FROM workers
+		WHERE id = $1
+	`
+
+	if _, err := r.Client.Exec(ctx, query, uuid); err != nil {
+		log.Printf("error deleting worker: %v\n", err)
+
+		return err
+	}
+
+	return nil
 }
