239e9820ce3c2d46b51452f0d33e13538d6930dc
lly <pinklywmp@gmail.com>
2026-02-04 19:48:17 +0300

    added delete handler

--- internal/adapters/web/workersHandler.go
+++ internal/adapters/web/workersHandler.go
@@ -23,13 +23,13 @@ func NewHandler(workerService services.WorkerService) Handler {
 }
 
 func (h *workerHandler) Register(router *gin.Engine) {
-	workers := router.Group("listWorkers")
+	workers := router.Group("listworkers")
 	{
 		workers.POST(loginURL)
 		workers.GET("/:department", h.ListDepartmentHandler)
 		workers.POST("/:department", h.Addworker)
 		workers.PUT("/:id")
-		workers.DELETE("/:id")
+		workers.DELETE("/:id", h.DeleteWorker)
 	}
 }
 
@@ -59,6 +59,7 @@ func (h *workerHandler) ListDepartmentHandler(c *gin.Context) {
 }
 
 func (h *workerHandler) Addworker(c *gin.Context) {
+	departmentName := c.Param("department")
 	var worker payloads.WorkerPayload
 
 	if err := c.BindJSON(&worker); err != nil {
@@ -71,7 +72,7 @@ func (h *workerHandler) Addworker(c *gin.Context) {
 		return
 	}
 
-	newWorker, err := h.workerService.CreateWorker(worker.Name, worker.JobTitle, worker.Department, worker.Password)
+	newWorker, err := h.workerService.CreateWorker(worker.Name, worker.JobTitle, departmentName, worker.Password)
 	if err != nil {
 		log.Printf("server error: %v", err)
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -84,5 +85,29 @@ func (h *workerHandler) Addworker(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "new worker was added",
 		"worker":  newWorker,
+	})
+}
+
+func (h *workerHandler) DeleteWorker(c *gin.Context) {
+	workerUUID := c.Param("id")
+
+	if len(workerUUID) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "bad request",
+		})
+
+		return
+	}
+
+	if err := h.workerService.DeleteWorkerByUUID(workerUUID); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "internal server error",
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "worker was successfully deleted",
 	})
 }
