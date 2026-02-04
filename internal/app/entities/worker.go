3decc0e4555863655d6af4e9ad5847433ce979e9
lly <pinklywmp@gmail.com>
2026-02-04 18:31:41 +0300

    remove link to struct from init func and added db bind for fields

--- internal/app/entities/worker.go
+++ internal/app/entities/worker.go
@@ -2,14 +2,14 @@ package entities
 
 type Worker struct {
 	UUID       string
-	Name       string `json:"name"`
-	JobTitle   string `json:"jobtitle"`
-	Department string `json:"department"`
-	Password   string `json:"password"`
-	AcessLevel int    `json:"acesslevel"`
+	Name       string `json:"name" db:"name"`
+	JobTitle   string `json:"jobtitle" db:"jobtitle"`
+	Department string `json:"department" db:"department"`
+	Password   string `json:"password" db:"password"`
+	AcessLevel int    `json:"acesslevel" db:"accesslevel"`
 }
 
-func (w *Worker) NewWorker(name, jobTitle, department, password string, accessLevel int) *Worker {
+func NewWorker(name, jobTitle, department, password string, accessLevel int) *Worker {
 	return &Worker{
 		Name:       name,
 		JobTitle:   jobTitle,
