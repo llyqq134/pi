903e764f25069f3186f1344471ab1c16c2da1f83
lly <pinklywmp@gmail.com>
2026-02-04 19:49:36 +0300

    db conf

--- internal/config/db.go
+++ internal/config/db.go
@@ -1,9 +1,9 @@
 package config
 
 type ConfigDatabase struct {
-	Host string `yaml:"host" env:"DB_HOST" env-default:"localhost"`
-	Port string `yaml:"port" env:"DB_PORT" env-default:"5432"`
-	Name string `yaml:"user" env:"DB_USER" env-required:"true"`
-	User string `yaml:"user" env:"DB_USER" env-required:"true"`
+	Host     string `yaml:"host" env:"DB_HOST" env-default:"localhost"`
+	Port     string `yaml:"port" env:"DB_PORT" env-default:"5432"`
+	Name     string `yaml:"name" env:"DB_NAME" env-required:"true"`
+	User     string `yaml:"user" env:"DB_USER" env-required:"true"`
 	Password string `yaml:"password" env:"DB_PASSWORD" env-required:"true"`
 }
