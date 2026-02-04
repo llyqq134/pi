9129edcfcd9a9f8d2e8996a209c351374c8ba2b9
lly <pinklywmp@gmail.com>
2026-02-04 18:29:12 +0300

    remove pointer in return value of GetById

--- internal/app/interfaces/services/workerService.go
+++ internal/app/interfaces/services/workerService.go
@@ -4,7 +4,7 @@ import "pi/internal/app/entities"
 
 type WorkerService interface {
 	CreateWorker(name, jobTitle, departament, password string) (*entities.Worker, error)
-	GetWokerByUUID(uuid string) (*entities.Worker, error)
+	GetWokerByUUID(uuid string) (entities.Worker, error)
 	GetAllWorkers() ([]entities.Worker, error)
 	GetAllWorkersByDepartment(department string) ([]entities.Worker, error)
 }
