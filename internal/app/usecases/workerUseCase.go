b4adf382165cf0f72cb5260eb926cfae68bd252b
lly <pinklywmp@gmail.com>
2026-02-04 18:29:43 +0300

    useCases for workers

--- internal/app/usecases/workerUseCase.go
+++ internal/app/usecases/workerUseCase.go
@@ -1,6 +1,7 @@
 package usecases
 
 import (
+	"context"
 	"pi/internal/app/entities"
 	repointerfaces "pi/internal/app/interfaces/repoInterfaces"
 	"pi/internal/app/interfaces/services"
@@ -15,9 +16,30 @@ func NewWorkerService(repo repointerfaces.WorkerRepo) services.WorkerService {
 }
 
 func (s *workerService) CreateWorker(name, jobTitle, departament, password string) (*entities.Worker, error) {
-	if jobTitle == 
-	worker := entities.NewWorker()
+	var accesslevel int
+
+	switch jobTitle {
+	case "manager":
+		accesslevel = 2
+	case "admin":
+		accesslevel = 3
+	default:
+		accesslevel = 1
+	}
+
+	worker := entities.NewWorker(name, jobTitle, departament, password, accesslevel)
+
+	return worker, s.repo.Create(context.Background(), worker)
 }
-func (s *workerService) GetWokerByUUID(uuid string) (*entities.Worker, error)
-func (s *workerService) GetAllWorkers() ([]entities.Worker, error)
-func (s *workerService) GetAllWorkersByDepartment(department string) ([]entities.Worker, error)
+
+func (s *workerService) GetWokerByUUID(uuid string) (entities.Worker, error) {
+	return s.repo.GetByUUID(context.Background(), uuid)
+}
+
+func (s *workerService) GetAllWorkers() ([]entities.Worker, error) {
+	return s.repo.GetAll(context.Background())
+}
+
+func (s *workerService) GetAllWorkersByDepartment(department string) ([]entities.Worker, error) {
+	return s.repo.GetAllByDepartment(context.Background(), department)
+}
